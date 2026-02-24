28c10faf1e1eb5d7d0dea5b60ab7a0dd57ae3e9b
Anmol Saxena <anmol.25161036@kiet.edu>
2026-02-24 22:15:18 +0530

    Add files via upload

--- 24-02-26/MainSealed.java
+++ 24-02-26/MainSealed.java
@@ -1,1 +1,21 @@
+package SealedClas;
+sealed class Shape permits Circle, Rectangle{
+	
+}
+//Non-permissible class cannot extend the sealed class as it is not a permitted sub-type of Shape
+final class Circle extends Shape{
+	
+}
 
+non-sealed class Rectangle extends Shape{
+	
+}
+
+public class MainSealed {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
